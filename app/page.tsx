bd4e80190f48767a170198bece64c22dc96f0c5b
Ch3tanUppin <sathyprakashuppin@gmail.com>
2024-10-27 14:58:54 +0530

    Minor Changes

--- app/page.tsx
+++ app/page.tsx
@@ -62,8 +62,7 @@ export default function Home() {
          <Globe/>
           <div className='max-w-[1280px] mx-auto relative z-[2]  px-5'>
             <ul className='lg:w-9/12 text-[14px] pt-16 text-[#7d8590] space-y-1'>
-              <li><sup id="footnote-1">1</sup> The Total Economic Impact™ OfZidioEnterprise Cloud and Advanced Security, a commissioned study conducted by Forrester Consulting, 2022. Results are for a composite organization based on interviewed customers.</li>
-              <li><sup id="footnote-2">2</sup> GitHub, Octoverse 2022 The state of open source software.</li>
+              <li><sup id="footnote-1">1</sup> The Total Economic Impact™ Of Zidio Enterprise Cloud and Advanced Security, a commissioned study conducted by Forrester Consulting, 2022. Results are for a composite organization based on interviewed customers.</li>
             </ul>
           </div>
         </div>
